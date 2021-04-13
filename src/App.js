be529fd53e7849e621c873469b4373dca0912fb7
Nguyen Do Cuong <34064401+docuong0912@users.noreply.github.com>
2021-04-14 02:53:27 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import { Cars } from './data/Cars';
+function App() {
+    
+   
+    return (
+        <div className="container-fluid text-white mt-5 d-lg-flex mx-auto p-lg-5 col-lg-9" >
+            {
+                
+                Cars.map((info) => {
+                    
+                    return (
+                        <div key={info.id} className={`car-${info.name} p-5 col-lg-4`}>
+                            <img src={info.src} alt="car" />
+                            <h1 className="font my-4 text-uppercase">{info.name}</h1>
+                            <p className="line-break ">{info.status}</p>
+                            <input className={`button button-${info.name} mt-lg-5`} type='submit' value='Learn More' />
+                    </div>);
+                }
+            )}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/data/Cars.js
@@ -0,0 +1,25 @@
+// JavaScript source code
+import  sedans  from '../images/icon-sedans.svg';
+import  suvs  from '../images/icon-suvs.svg';
+import  luxury  from '../images/icon-luxury.svg';
+
+export const Cars = [
+    {
+        id: 1,
+        name:'sedans',
+        status:'Choose a sedan for its affordability and excellent fuel economy. Idea for cruising in the city or on your next road trip.',
+        src:sedans,
+    },
+    {
+        id: 2,
+        name: 'suvs',
+        status: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off- road adventures. ',
+        src:suvs,
+    }, 
+    {
+        id: 3,
+        name: 'luxury',
+        status: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+        src: luxury,
+    },
+]
